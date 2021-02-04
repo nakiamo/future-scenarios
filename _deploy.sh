b1b7b990b9f0ab804771084b07124744120fb11b
Nakiamo <naimcinar@gmail.com>
2021-02-04 17:40:00 +0300

    first

--- _deploy.sh
+++ _deploy.sh
@@ -8,9 +8,13 @@ set -e
 git config --global user.email "naimcinar@gmail.com"
 git config --global user.name "Nakiamo"
 
-git clone -b gh-pages https://${GITHUB_PAT}@github.com/${nakiamo/future-scenarios-book}.git book-output
+git clone -b gh-pages \
+  https://${GITHUB_PAT}@github.com/${nakiamo/future-scenarios-book}.git \
+  book-output
 cd book-output
+git rm -rf *
 cp -r ../_book/* ./
 git add --all *
-git commit -m"Update the book" || true
+git commit -m"Update the book"
 git push -q origin gh-pages
+
