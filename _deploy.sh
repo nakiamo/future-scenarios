fbbdbf34010172d893c3c27a002707ee56177057
Nakiamo <naimcinar@gmail.com>
2021-02-04 15:49:42 +0300

    first

---
+++ _deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+[ -z "${GITHUB_PAT}" ] && exit 0
+[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
+
+git config --global user.email "naimcinar@gmail.com"
+git config --global user.name "Nakiamo"
+
+git clone -b gh-pages https://${GITHUB_PAT}@github.com/${nakiamo/future-scenarios-book}.git book-output
+cd book-output
+cp -r ../_book/* ./
+git add --all *
+git commit -m"Update the book" || true
+git push -q origin gh-pages
